b44485c5c38c336872f3e1640679e61ea1b9d48c
KNU-K <knukang334@gmail.com>
2025-02-24 04:07:09 +0900

    feat : initialize the project

---
+++ src/main/java/practica/tps_analysis/TpsAnalysisPracticaApplication.java
@@ -0,0 +1,13 @@
+package practica.tps_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpsAnalysisPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpsAnalysisPracticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practica/tps_analysis/TpsAnalysisPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package practica.tps_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpsAnalysisPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
